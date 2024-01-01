b1a2011241a036cbdc7d205d7e89ff1b24013d8d
NiklasEiler <niklaseilersausheinde@gmail.com>
2024-01-01 18:02:11 +0100

    3D_Data

--- src/my_pointcloud/setup.py
+++ src/my_pointcloud/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            "3d_node=my_3d_node.my_3d_node:main"
+            "3d_node=my_pointcloud.my_3d_node:main"
         ],
     },
 )
